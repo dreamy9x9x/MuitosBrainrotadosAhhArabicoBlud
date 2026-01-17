8c80830e32a876f76eb8c7232a378cbff5cc8607
DreamyRobloxRants <dreamytubers@gmail.com>
2026-01-17 20:36:16 -0300

    Rename variable targetPlayer to MatemOsUrsosPolares

---
+++ espamafemilyiviteAAUU.lua
@@ -0,0 +1,16 @@
+local svcPlayers = game:GetService("Players")
+local svcStorage = game:GetService("ReplicatedStorage")
+
+local evtInvite = svcStorage
+	:WaitForChild("Remotes")
+	:WaitForChild("InvitePlayerToFamily")
+
+for attempt = 1, 100 do
+	task.spawn(function()
+		for _, MatemOsUrsosPolares in ipairs(svcPlayers:GetPlayers()) do
+			if targetPlayer ~= svcPlayers.LocalPlayer then
+				evtInvite:FireServer(MatemOsUrsosPolares.UserId, "Wheel")
+			end
+		end
+	end)
+end
